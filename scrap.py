0285cad9e495df26a6bd4b28e4905b11d914963e
cama-ka <karlacamarena91@yahoo.fr>
2021-04-06 21:15:48 +0200

    debug decalage colonne

--- scrap.py
+++ scrap.py
@@ -2,12 +2,14 @@ import os
 import requests
 from bs4 import BeautifulSoup
 
+
 URL = "https://books.toscrape.com/index.html"
 
 def creation_repertoires():
     # Je cree deux repertoires : un pour les images et un pour les csv.
     try:
         os.mkdir('books/')
+        os.mkdir('images/')
     except OSError:
         pass
     else:
@@ -35,7 +37,7 @@ def pagination(lien, cat):
 def lien_livre(lien, cat):
         r = requests.get(lien)
         if r.ok: 
-            lien = BeautifulSoup(r.content,features='html.parser')
+            lien = BeautifulSoup(r.content, "lxml")
             for li in lien.find_all('li', {'class': 'col-xs-6 col-sm-4 col-md-3 col-lg-3'}):
                 for h3 in li.find_all('h3'):
                     lien_livre = h3.a.attrs['href']
@@ -48,21 +50,28 @@ def info_livre(url_du_livre, cat):
     print(url_du_livre)
     response = requests.get(url_du_livre)
     if response.ok:
-            with open('books/' + cat + '.csv','a', encoding='utf-8') as file: #j'ouvre le fichier books + cat qui changera a chaque fois + le type de fichier, en mode append
-                soup = BeautifulSoup(response.content, features="html.parser")
-                tds = soup.find_all('td')
-                universalProductCode = tds[0].text.replace(',', '').replace(';', '')
-                priceIncludingTax = tds[3].text.replace(',', '').replace(';', '')
-                priceExcludingTax = tds[2].text.replace(',', '').replace(';', '')
-                numberAvailable = tds[5].text.replace(',', '').replace(';', '')
-                reviewRating = tds[6].text.replace(',', '').replace(';', '')
-                title = soup.find('div', {'class': 'col-sm-6 product_main'}).find('h1').text.replace(',', '').replace(';', '')
-                productDescription = soup.find('article', {'class': 'product_page'}).find_all('p')[3].text.replace(',', '').replace(';', '')
-                category = soup.find('ul', {'class': 'breadcrumb'}).find_all('a')[2].text.replace(',', '').replace(';', '')
-                imageUrl = soup.find('div', {'class': 'item active'}).find('img').attrs['src'].replace('../..', 'http://books.toscrape.com')
-                file.write(url_du_livre + ';' + universalProductCode + ';' + title + ';' + priceIncludingTax + ';' + priceExcludingTax + ';' + numberAvailable + ';' + productDescription + ';' + category + ';' + reviewRating + ';' + imageUrl + '\n')
+        with open('books/' + cat + '.csv', 'a', encoding='utf-8-sig') as file: #j'ouvre le fichier books + cat qui changera a chaque fois + le type de fichier, en mode append
+            soup = BeautifulSoup(response.content, "lxml")
+            tds = soup.find_all('td')
+            universalProductCode = tds[0].text.replace(',', '').replace(';', '')
+            priceIncludingTax = tds[3].text.replace(',', '').replace(';', '')
+            priceExcludingTax = tds[2].text.replace(',', '').replace(';', '')
+            numberAvailable = tds[5].text.replace(',', '').replace(';', '')
+            reviewRating = tds[6].text.replace(',', '').replace(';', '')
+            title = soup.find('div', {'class': 'col-sm-6 product_main'}).find('h1').text.replace(',', '').replace(';', '')
+            productDescription = soup.find('article', {'class': 'product_page'}).find_all('p')[3].text.replace(',', '').replace(';', '')
+            category = soup.find('ul', {'class': 'breadcrumb'}).find_all('a')[2].text.replace(',', '').replace(';', '')
+            imageUrl = soup.find('div', {'class': 'item active'}).find('img').attrs['src'].replace('../..', 'http://books.toscrape.com')
+            imageDownload(imageUrl, universalProductCode)
+            file.write(url_du_livre + ';' + universalProductCode + ';' + title + ';' + priceIncludingTax + ';' + priceExcludingTax + ';' + numberAvailable + ';' + productDescription + ';' + category + ';' + reviewRating + ';' + imageUrl + '\n')
 
-
+def imageDownload(imageUrl, productCode):
+	response = requests.get(imageUrl)
+	file = open('images/' + productCode + '.jpg' , "wb")
+	file.write(response.content)
+	file.close()
+    
+    
 def main():
     """ Je cree la fonction principale.\
         Elle va activer les autres fonctions et remplir les fichiers csv."""
@@ -71,16 +80,16 @@ def main():
     # Il faut trouver les liens de chaque categorie.
     response = requests.get(URL)
     if response.ok:
-        soup = BeautifulSoup(response.content, features="html.parser")
+        soup = BeautifulSoup(response.content, "lxml")
         for ultag in soup.find_all('ul', {'class': 'nav nav-list'}):
             ultagul = ultag.find('ul')    
             for litag in ultagul.find_all('li'):
                 linkCategory = litag.a.attrs['href']
                 cat = linkCategory.replace("catalogue/category/books/","").replace('/index.html','')
                 lien = "https://books.toscrape.com/" + linkCategory
-                with open('books/' + cat + '.csv', 'w', encoding="utf-8") as file:
-                    file.write('product_page_url;universal_product_code;title;price_including_tax;price_excluding_tax;number_available;product_description;category;review_rating;image_url\n')
-                    pagination(lien, cat)
+                with open('books/' + cat + '.csv', 'w', encoding='utf-8-sig') as file:
+                    file.write('product_page_url;universal_product_code;title;price_including_tax;price_excluding_tax;number_available;product_description;category;review_rating;image_url;\n')
+                pagination(lien, cat)
                         
     
 if __name__ == "__main__":
