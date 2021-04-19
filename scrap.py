a9f16f23388134d346a15766d0a12155ca25ec94
cama-ka <karlacamarena91@yahoo.fr>
2021-04-19 20:33:50 +0200

    liaison scrap et scrap_one + dictionaire

--- scrap.py
+++ scrap.py
@@ -1,12 +1,11 @@
 import os
 import requests
 from bs4 import BeautifulSoup
+from scrap_one import book_one
 
-
-URL = "https://books.toscrape.com/index.html"
 
 def creation_repertoires():
-    # Creating two folders : books and images
+    ''' Creating two folders : books and images '''
     try:
         os.mkdir('books/')
         os.mkdir('images/')
@@ -16,16 +15,15 @@ def creation_repertoires():
         pass
         
                     
-def pagination(lien, cat):
-    # We have to go into every pages of a category
+def pagination(lien, cat, URL):
+    ''' We have to go into every pages of a category '''
     lien = lien.replace("index.html", "page-1.html")
     r = requests.get(lien)
     if r.ok:
     # If http:// xxx.page-1.html exist, we find her and replace the 1 and go for the second page.
         i = 1
         while r.ok:
-        # While the url is ok, got to the function lien_livre
-            lien_livre(lien, cat)
+            lien_livre(lien, cat, URL)
             # replace the number of the page until the response is not ok
             i += 1
             lien = lien.replace("page-" + str(i - 1) + ".html", "page-" + str(i) + ".html")
@@ -35,11 +33,11 @@ def pagination(lien, cat):
         lien = lien.replace("page-1.html", "index.html")
         r = requests.get(lien)
         if r.ok:
-            lien_livre(lien, cat)
+            lien_livre(lien, cat, URL)
                     
 
-def lien_livre(lien, cat):
-    # Go to url of the category.
+def lien_livre(lien, cat, URL):
+    ''' Go to url of the category. '''
     r = requests.get(lien)
     if r.ok: 
         lien = BeautifulSoup(r.content, "lxml")
@@ -49,62 +47,10 @@ def lien_livre(lien, cat):
                 url_du_livre = URL.replace('index.html','') + \
                     lien_livre.replace('../../../','catalogue/').replace('../../','catalogue/')
                     # Find the link of a book and execute the info_livre function
-                info_livre(url_du_livre, cat)
+                book_one(url_du_livre, cat)
                     
-
-def info_livre(url_du_livre, cat):
-    # For each book, take the informations needed and write them in the csv file we made before on the main function.
-    print(url_du_livre)
-    response = requests.get(url_du_livre)
-    if response.ok:
-        with open('books/' + cat + '.csv', 'a', encoding='utf-8-sig') as file: 
-            #j'ouvre le fichier books + cat qui changera a chaque fois + le type de fichier, en mode append
-            soup = BeautifulSoup(response.content, "lxml")
-            tds = soup.find_all('td')
-            universal_product_code = tds[0].text.replace(',', '').replace(';', '')
-            price_including_tax = tds[3].text.replace(',', '').replace(';', '')
-            price_excluding_tax = tds[2].text.replace(',', '').replace(';', '')
-            number_available = tds[5].text.replace(',', '').replace(';', '')
-            p = soup.find_all('p')
-            review = str(p[2])
-            review_rating = review[0:30]
-            if "One" in review_rating:
-                review_rating = "1"
-            elif "Two" in review_rating:
-                review_rating = "2"
-            elif "Three" in review_rating:
-                review_rating = "3"
-            elif "Four" in review_rating:
-                review_rating = "4"
-            elif "Five" in review_rating:
-                review_rating = "5"
-            title = soup.find('div', {'class': 'col-sm-6 product_main'}).\
-                find('h1').text.replace(',', '').replace(';', '')
-            product_description = soup.find('article', {'class': 'product_page'}).\
-                find_all('p')[3].text.replace(',', '').replace(';', '')
-            category = soup.find('ul', {'class': 'breadcrumb'}).find_all('a')[2].\
-                text.replace(',', '').replace(';', '')
-            image_url = soup.find('div', {'class': 'item active'}).find('img').\
-                attrs['src'].replace('../..', 'http://books.toscrape.com')
-            image_download(image_url, universal_product_code)
-            path = "images/" + universal_product_code + ".jpg"
-            if os.path.exists:
-                image_path = path
-            else:
-                image_path = "doesn't exist"
-            file.write(url_du_livre + ';' + universal_product_code + ';' + title +\
-                ';' + price_including_tax + ';' + price_excluding_tax + ';' + number_available +\
-                ';' + product_description + ';' + category + ';' + review_rating + ';' + image_url + ';' + image_path + '\n')
-
-def image_download(image_url, product_code):
-# Uploading images of each book in the folder we created before.
-	response = requests.get(image_url)
-	file = open('images/' + product_code + '.jpg' , "wb")
-	file.write(response.content)
-	file.close()
     
-    
-def main():
+def main(URL):
     """ The main function it's gonna find links for each categorys,\
         creat csv files and activate the others function"""
     creation_repertoires()
@@ -122,8 +68,8 @@ def main():
                     file.write('product_page_url;universal_product_code;title;price_including_tax;'
                     'price_excluding_tax;number_available;product_description;category;'
                     'review_rating;image_url;image_path;\n')
-                pagination(lien, cat)
+                pagination(lien, cat, URL)
                         
     
 if __name__ == "__main__":
-    main()
+    main("https://books.toscrape.com/index.html")
--- scrap_one.py
+++ scrap_one.py
@@ -3,57 +3,51 @@ import requests
 from bs4 import BeautifulSoup
 
 
-url_du_livre = "https://books.toscrape.com/catalogue/olio_984/index.html"
-
 def creation_repertoires():
     # Creating folder
     try:
-        os.mkdir('one_book/')
-        os.mkdir('image/')
+        os.mkdir('books/')
+        os.mkdir('images/')
     except OSError:
         pass
     else:
         pass
 
-def book_one():
+def book_one(url_du_livre, cat):
     response = requests.get(url_du_livre)
     if response.ok:
         print(url_du_livre)
-        with open('one_book/' + 'olio' + '.csv', 'a', encoding='utf-8-sig') as file:
-            soup = BeautifulSoup(response.content, "lxml")
-            tds = soup.find_all('td')
-            universal_product_code = tds[0].text.replace(',', '').replace(';', '')
-            price_including_tax = tds[3].text.replace(',', '').replace(';', '')
-            price_excluding_tax = tds[2].text.replace(',', '').replace(';', '')
-            number_available = tds[5].text.replace(',', '').replace(';', '')
-            p = soup.find_all('p')
-            review = str(p[2])
-            review_rating = review[0:30]
-            if "One" in review_rating:
-                review_rating = "1"
-            elif "Two" in review_rating:
-                review_rating = "2"
-            elif "Three" in review_rating:
-                review_rating = "3"
-            elif "Four" in review_rating:
-                review_rating = "4"
-            elif "Five" in review_rating:
-                review_rating = "5"
-            title = soup.find('div', {'class': 'col-sm-6 product_main'}).\
-                find('h1').text.replace(',', '').replace(';', '')
-            product_description = soup.find('article', {'class': 'product_page'}).\
-                find_all('p')[3].text.replace(',', '').replace(';', '')
-            category = soup.find('ul', {'class': 'breadcrumb'}).find_all('a')[2].\
-                text.replace(',', '').replace(';', '')
-            image_url = soup.find('div', {'class': 'item active'}).find('img').\
-                attrs['src'].replace('../..', 'http://books.toscrape.com')
-            image_download(image_url, universal_product_code)
-            path = "image/" + universal_product_code + ".jpg"
-            if os.path.exists:
-                image_path = path
-            else : 
-                image_path = "doesn't exist"
-            print(image_path)
+        soup = BeautifulSoup(response.content, "lxml")
+        tds = soup.find_all('td')
+        universal_product_code = tds[0].text.replace(',', '').replace(';', '')
+        price_including_tax = tds[3].text.replace(',', '').replace(';', '')
+        price_excluding_tax = tds[2].text.replace(',', '').replace(';', '')
+        number_available = tds[5].text.replace(',', '').replace(';', '')
+        
+        dic = {"One": 1, "Two": 2, "Three": 3, "Four": 4, "Five": 5}
+        review_rating = soup.find('p', {'class': 'star-rating'})['class'][1]
+        for clef in dic.keys(): 
+            if clef == review_rating:
+                review_rating = str(dic[clef])
+            else:
+                pass
+            
+        title = soup.find('div', {'class': 'col-sm-6 product_main'}).\
+            find('h1').text.replace(',', '').replace(';', '')
+        product_description = soup.find('article', {'class': 'product_page'}).\
+            find_all('p')[3].text.replace(',', '').replace(';', '')
+        category = soup.find('ul', {'class': 'breadcrumb'}).find_all('a')[2].\
+            text.replace(',', '').replace(';', '')
+        image_url = soup.find('div', {'class': 'item active'}).find('img').\
+            attrs['src'].replace('../..', 'http://books.toscrape.com')
+        image_download(image_url, universal_product_code)
+        path = "images/" + universal_product_code + ".jpg"
+        if os.path.exists:
+            image_path = path
+        else : 
+            image_path = "doesn't exist"
+
+        with open('books/' + cat + '.csv', 'a', encoding='utf-8-sig') as file:
             file.write(url_du_livre + ';' + universal_product_code + ';' + title +\
                 ';' + price_including_tax + ';' + price_excluding_tax + ';' + number_available +\
                 ';' + product_description + ';' + category + ';' + review_rating + ';' + image_url + ';' + image_path +'\n')
@@ -61,24 +55,23 @@ def book_one():
     
 def image_download(image_url, product_code):
 # Uploading images of each book in the folder we created before.
-	response = requests.get(image_url)
-	file = open('image/' + product_code + '.jpg' , "wb")
-	file.write(response.content)
-	file.close()
+    response = requests.get(image_url)
+    with open('images/' + product_code + '.jpg' , "wb") as file:
+        file.write(response.content)
     
     
-def main():
+def main(cat):
     """ The main function it's gonna find links for each categorys,\
         creat csv files and activate the others function"""
     creation_repertoires()
     
-    with open('one_book/' + 'olio' + '.csv', 'w', encoding='utf-8-sig') as file:
+    with open('books/' + cat + '.csv', 'w', encoding='utf-8-sig') as file:
         file.write('product_page_url;universal_product_code;title;price_including_tax;'
         'price_excluding_tax;number_available;product_description;category;'
         'review_rating;image_url;image_path;\n')
         
-    book_one()
+    book_one("https://books.toscrape.com/catalogue/a-light-in-the-attic_1000/index.html", cat)
                         
     
 if __name__ == "__main__":
-    main()
+    main("Un_livre")
