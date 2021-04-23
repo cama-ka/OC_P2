604327b01f91ec49e52f93e53f2199f1c47b75f4
cama-ka <karlacamarena91@yahoo.fr>
2021-04-23 11:45:57 +0200

    ecriture infos

--- scrap.py
+++ scrap.py
@@ -1,7 +1,9 @@
 import os
 import requests
 from bs4 import BeautifulSoup
+import csv
 from scrap_one import book_one
+from scrap_one import ecriture_info
 
 
 def creation_repertoires():
@@ -20,16 +22,13 @@ def pagination(lien, cat, URL):
     lien = lien.replace("index.html", "page-1.html")
     r = requests.get(lien)
     if r.ok:
-    # If http:// xxx.page-1.html exist, we find her and replace the 1 and go for the second page.
         i = 1
         while r.ok:
             lien_livre(lien, cat, URL)
-            # replace the number of the page until the response is not ok
             i += 1
             lien = lien.replace("page-" + str(i - 1) + ".html", "page-" + str(i) + ".html")
             r = requests.get(lien)   
     else:
-    # If the url is not ok, don't change and execute lien_livre function
         lien = lien.replace("page-1.html", "index.html")
         r = requests.get(lien)
         if r.ok:
@@ -46,9 +45,10 @@ def lien_livre(lien, cat, URL):
                 lien_livre = h3.a.attrs['href']
                 url_du_livre = URL.replace('index.html','') + \
                     lien_livre.replace('../../../','catalogue/').replace('../../','catalogue/')
-                    # Find the link of a book and execute the info_livre function
+                print(url_du_livre)
                 book_one(url_du_livre, cat)
-                    
+                ecriture_info(url_du_livre, cat)
+                   
     
 def main(URL):
     """ The main function it's gonna find links for each categorys,\
@@ -59,17 +59,19 @@ def main(URL):
     if response.ok:
         soup = BeautifulSoup(response.content, "lxml")
         for ultag in soup.find_all('ul', {'class': 'nav nav-list'}):
-            ultagul = ultag.find('ul')    
+            ultagul = ultag.find('ul')
             for litag in ultagul.find_all('li'):
                 link_category = litag.a.attrs['href']
                 cat = link_category.replace("catalogue/category/books/","").replace('/index.html','')
                 lien = "https://books.toscrape.com/" + link_category
                 with open('books/' + cat + '.csv', 'w', encoding='utf-8-sig') as file:
+                    csv_writer = csv.writer(file, delimiter=';')
                     file.write('product_page_url;universal_product_code;title;price_including_tax;'
                     'price_excluding_tax;number_available;product_description;category;'
                     'review_rating;image_url;image_path;\n')
+                    print(f"{cat} est en cours de téléchargement")
                 pagination(lien, cat, URL)
-                        
+                       
     
 if __name__ == "__main__":
     main("https://books.toscrape.com/index.html")
