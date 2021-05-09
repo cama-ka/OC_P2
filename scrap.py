7181bdd6ff899d97337b2837e2e93e446bec1d7b
cama-ka <karlacamarena91@yahoo.fr>
2021-05-09 20:40:02 +0200

    correction f-string

--- scrap.py
+++ scrap.py
@@ -5,7 +5,8 @@ import csv
 from scrap_one import book_one
 from scrap_one import ecriture_info
 from scrap_one import creation_repertoires
-        
+from scrap_one import category
+
                     
 def pagination(lien, cat, URL):
     ''' Go into every pages of a category '''
@@ -16,7 +17,8 @@ def pagination(lien, cat, URL):
         while r.ok:
             lien_livre(lien, cat, URL)
             i += 1
-            lien = lien.replace("page-" + str(i - 1) + ".html", "page-" + str(i) + ".html")
+            y = 1
+            lien = lien.replace(f"page-{i-y}.html", f"page-{i}.html")
             r = requests.get(lien)   
     else:
         lien = lien.replace("page-1.html", "index.html")
@@ -26,7 +28,7 @@ def pagination(lien, cat, URL):
                     
 
 def lien_livre(lien, cat, URL):
-    ''' Go to url of the category. '''
+    ''' Go to the url of the category. '''
     r = requests.get(lien)
     if r.ok: 
         lien = BeautifulSoup(r.content, "lxml")
@@ -35,15 +37,16 @@ def lien_livre(lien, cat, URL):
                 lien_livre = h3.a.attrs['href']
                 url_du_livre = URL.replace('index.html','') + \
                     lien_livre.replace('../../../','catalogue/').replace('../../','catalogue/')
-                print(url_du_livre)
+                nom_du_livre = url_du_livre.replace('https://books.toscrape.com/catalogue/','').replace('/index.html','')
                 book_one(url_du_livre, cat)
                 ecriture_info(url_du_livre, cat)
     else:
         print("The page cannot be find.")
+        pass
                    
     
 def main(URL):
-    """ Find links for each categorys, create csv files and activate the others function"""
+    """ Find links for each categories, activate the others function """
     creation_repertoires('books/')
     creation_repertoires('images/')
     
@@ -56,15 +59,12 @@ def main(URL):
                 link_category = litag.a.attrs['href']
                 cat = link_category.replace("catalogue/category/books/","").replace('/index.html','')
                 lien = "https://books.toscrape.com/" + link_category
-                with open(f"books/ {cat} .csv", 'w', encoding='utf-8-sig', newline="") as csvfile:
-                    fieldnames = ["product_page_url","universal_product_code","title","price_including_tax",\
-                                "price_excluding_tax","number_available","product_description","category",\
-                                "review_rating","image_url","image_path"]
-                    writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-                    writer.writeheader()
+                category(cat)
+                print(f"La catégorie {cat} est en cours d'écriture. Le fichier devrait être visible dans le dossier source.")
                 pagination(lien, cat, URL)
     else:
         print("The page cannot be find.")
+        pass
                        
     
 if __name__ == "__main__":
